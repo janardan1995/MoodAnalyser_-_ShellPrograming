b54741a4f4a224ded1f6b5d529c8134d4f67d57c
janardan1995 <dasjanardan1995@gmail.com>
2020-02-22 10:26:53 +0530

    [JD]Add. Use case 2 is created for exception handleing in moodAnalysis program

--- MoodAnalyser/MoodAnalyserProgram.cs
+++ MoodAnalyser/MoodAnalyserProgram.cs
@@ -10,11 +10,47 @@ namespace Class1
     using System;
     public class MoodAnalyser
     {
-        public static string AnalysisMoodFunction(string str)
-        {                      
-            if (str.Contains("sad"))
+      
+        public string str;
+       
+        public MoodAnalyser(string message)
+        {
+            this.str = message;
+        }
+
+        
+        public static string AnalysisMoodFunction(string strr)
+        {
+            if (strr.ToLower().Contains("sad"))
             {
                 return "Sad mood";
+            }
+            return "Happy";
+        }   
+      
+        public string AnalysisMoodFunctionWithConstructor()
+        {           
+            if (str.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            return "Happy";
+            
+        }
+
+        public static string AnalysisMoodFunctionWithExceptionHandeling(string message)
+        {
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "sad";
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               // return "Happy";
             }
             return "Happy";
         }
--- MoodAnalyser/nUnitTestCases.cs
+++ MoodAnalyser/nUnitTestCases.cs
@@ -1,21 +1,100 @@
-using NUnit.Framework;
-using Class1;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="nUnitTestCase.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Janardan Das"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+/// <summary>
+/// this is the the nunit testing 
+/// </summary>
 namespace NUnitTestMoodAnalyser
 {
+    using NUnit.Framework;
+    using Class1;
+
+    /// <summary>
+    /// a Tests class is created
+    /// </summary>
     public class Tests
-    {       
+    {
+        /// <summary>
+        /// this is the first method to test for analysis mood should return sad
+        /// </summary>
         [Test]
-        public static void Giving_IaminSadMood_whenAnalysis_should_return_sad()
+        public void GivenSadMessage_WhenAnalyse_ShouldReturnSad()
         {
-            //arrange
-            string str = "I am in sad mood";
-            string excepted = "Sad mood";
+            try
+            {
+                ////arrange
+                string str = "I am in sad mood";
+                string excepted = "Sad mood";
 
-            //act
-            string match1 = MoodAnalyser.AnalysisMoodFunction(str);
+                ////act
+                string match1 = MoodAnalyser.AnalysisMoodFunction(str);
 
-            //Assert
-            Assert.AreEqual(excepted, match1);
+                ////Assert
+                Assert.AreEqual(excepted, match1);
+            }
+            catch (AssertionException e)
+            {
+                throw e;
+            }
+        }
+         
+        /// <summary>
+        /// this method should return happy if the string message has no sad word
+        /// </summary>
+        [Test]
+        public void GivenAnyMesageExceptSad_WhenAnalyse_ShouldReturnHappy()
+        {
+            ////arrange
+            string str = "I am in any mood";
+            string excepted = "Happy";
+
+            ////act
+            string match2 = MoodAnalyser.AnalysisMoodFunction(str);
+
+            ////Assert
+            Assert.AreEqual(excepted, match2);
+        }
+
+        /// <summary>
+        /// created a constructor in origin program 
+        /// here we have testing for sad 
+        /// </summary>
+        [Test]
+        public void GivenSadMessaageInConstructor_WhenAnalysis_ShouldReturnSad()
+        {
+            string expected = "sad";
+            MoodAnalyser ma =new MoodAnalyser("I am in sad Mood");
+            string x1 = ma.AnalysisMoodFunctionWithConstructor();
+            Assert.AreEqual(expected, x1);
+
+        }
+        
+        /// <summary>
+        /// created a constructor and should pass happy
+        /// </summary>
+        [Test]
+        public void GivenExceptSadMessaageInConstructor_WhenAnalysis_ShouldReturnHappy()
+        {
+            string expected = "Happy";
+            MoodAnalyser ma =new MoodAnalyser("I am in Happy Mood");
+            string x2 = ma.AnalysisMoodFunctionWithConstructor();
+            Assert.AreEqual(expected, x2);
+
+        }
+
+        /// <summary>
+        /// A method is created for exception and here tested
+        /// </summary>
+        [Test]
+        public void GivenNullMessage_whenAnalysis_ShouldReturnHappy()
+        {
+            string expected = "Happy";
+            var result = MoodAnalyser.AnalysisMoodFunctionWithExceptionHandeling(null);
+            Assert.AreEqual(expected,result);
         }
     }
 }
